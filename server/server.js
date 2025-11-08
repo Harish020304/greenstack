2248ad541d940b0d7360634e28680e7f8104697e
M K Harish kumar <154452148+Harish020304@users.noreply.github.com>
2025-11-08 21:02:02 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173',process.env.VITE_BACKEND_URL,'https://greencart-leqh.onrender.com']
+const allowedOrigins = ['http://localhost:5173',process.env.VITE_BACKEND_URL,'https://greenstack-two.vercel.app/']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
